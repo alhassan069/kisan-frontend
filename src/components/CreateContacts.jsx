5732ea0d10848546dd6a3fb61e2f20a70c04666f
Alimul Hassan <alhassan069@gmail.com>
2022-11-07 01:20:50 +0530

    changed once again css

--- src/components/CreateContacts.jsx
+++ src/components/CreateContacts.jsx
@@ -19,7 +19,6 @@ const CardWrapper = styled.div`
   font-family: Quicksand, arial, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
-  ${mobile({ width: "70%", padding: "0 0 16px" })}
 `;
 
 const CreateContacts = () => {
